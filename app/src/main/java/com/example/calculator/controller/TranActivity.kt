b57e899ada2d251add0fb85d6c365928ebe804ab
syx321 <1580925976@qq.com>
2021-10-02 18:30:47 +0800

    增加弹出式菜单

---
+++ app/src/main/java/com/example/calculator/controller/TranActivity.kt
@@ -0,0 +1,17 @@
+package com.example.calculator.controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calculator.R
+
+class TranActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tran)
+
+    }
+
+
+
+}
