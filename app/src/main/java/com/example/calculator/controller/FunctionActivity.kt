3caf50a37bb1071560289ce2da5cf1579cfc536d
syx321 <1580925976@qq.com>
2021-11-03 21:16:27 +0800

    [NewFeature] 完成单位转换controller 完成数学单位转换controller [Update] 优化数学单位转换UI

--- app/src/main/java/com/example/calculator/controller/FunctionActivity.kt
+++ app/src/main/java/com/example/calculator/controller/FunctionActivity.kt
@@ -2,13 +2,148 @@ package com.example.calculator.controller
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import android.view.View
 import com.example.calculator.R
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Button
+
+
+
 
 
 class FunctionActivity: AppCompatActivity() {
+    lateinit var sinButton: Button
+    lateinit var cosButton:Button
+    lateinit var binButton:Button
+    lateinit var octButton:Button
+    lateinit var decButton:Button
+    lateinit var hexButton:Button
+    lateinit var sinResult: TextView
+    lateinit var cosResult:TextView
+    lateinit var sinValue: EditText
+    lateinit var cosValue:EditText
+    lateinit var binValue:EditText
+    lateinit var octValue:EditText
+    lateinit var decValue:EditText
+    lateinit var hexValue:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_function)
+        initComponents()
     }
+
+    private fun initComponents() {
+        sinButton = findViewById(R.id.sin_button)
+        cosButton = findViewById<Button>(R.id.cos_button)
+        sinResult = findViewById(R.id.sinResult_text)
+        cosResult = findViewById<TextView>(R.id.cosResult_text)
+        sinValue = findViewById(R.id.sinValue_text)
+        cosValue = findViewById<EditText>(R.id.cosValue_text)
+        binButton = findViewById<Button>(R.id.bin_button)
+        octButton = findViewById<Button>(R.id.oct_button)
+        decButton = findViewById<Button>(R.id.dec_button)
+        hexButton = findViewById<Button>(R.id.hex_button)
+        binValue = findViewById<EditText>(R.id.binValue_text)
+        octValue = findViewById<EditText>(R.id.octValue_text)
+        decValue = findViewById<EditText>(R.id.decValue_text)
+        hexValue = findViewById<EditText>(R.id.hexValue_text)
+        sinButton.setOnClickListener(View.OnClickListener { sin() })
+        cosButton.setOnClickListener(View.OnClickListener { cos() })
+        binButton.setOnClickListener(View.OnClickListener { bin() })
+        octButton.setOnClickListener(View.OnClickListener { oct() })
+        decButton.setOnClickListener(View.OnClickListener { dec() })
+        hexButton.setOnClickListener(View.OnClickListener { hex() })
+    }
+
+    private fun sin() {
+        val s = sinValue.text.toString()
+        if (s.isEmpty()) {
+            return
+        }
+        val value = sinValue.text.toString().toDouble()
+        val result = Math.sin(value)
+        sinResult.text = result.toString()
+    }
+
+    private fun cos() {
+        val s: String = cosValue.getText().toString()
+        if (s.isEmpty()) {
+            return
+        }
+        val value: Double = cosValue.getText().toString().toDouble()
+        val result = Math.cos(value)
+        cosResult.text = result.toString()
+    }
+
+    private fun bin() {
+        val s: String = binValue.getText().toString()
+        if (s.isEmpty()) {
+            return
+        }
+        val value: Int
+        value = try {
+            Integer.valueOf(s, 2)
+        } catch (e: NumberFormatException) {
+            Toast.makeText(this, "输入格式有误，请重新输入", Toast.LENGTH_LONG).show()
+            return
+        }
+        octValue.setText(Integer.toOctalString(value))
+        hexValue.setText(Integer.toHexString(value))
+        decValue.setText(Integer.valueOf(value).toString())
+    }
+
+    private fun oct() {
+        val s: String = octValue.getText().toString()
+        if (s.isEmpty()) {
+            return
+        }
+        val value: Int
+        value = try {
+            Integer.valueOf(s, 8)
+        } catch (e: NumberFormatException) {
+            Toast.makeText(this, "输入格式有误，请重新输入", Toast.LENGTH_LONG).show()
+            return
+        }
+        binValue.setText(Integer.toBinaryString(value))
+        hexValue.setText(Integer.toHexString(value))
+        decValue.setText(Integer.valueOf(value).toString())
+    }
+
+    private fun dec() {
+        val s: String = decValue.getText().toString()
+        if (s.isEmpty()) {
+            return
+        }
+        val value: Int
+        value = try {
+            Integer.valueOf(s, 10)
+        } catch (e: NumberFormatException) {
+            Toast.makeText(this, "输入格式有误，请重新输入", Toast.LENGTH_LONG).show()
+            return
+        }
+        binValue.setText(Integer.toBinaryString(value))
+        hexValue.setText(Integer.toHexString(value))
+        octValue.setText(Integer.toOctalString(value))
+    }
+
+    private fun hex() {
+        val s: String = hexValue.getText().toString()
+        if (s.isEmpty()) {
+            return
+        }
+        val value: Int
+        value = try {
+            Integer.valueOf(s, 16)
+        } catch (e: NumberFormatException) {
+            Toast.makeText(this, "输入格式有误，请重新输入", Toast.LENGTH_LONG).show()
+            return
+        }
+        binValue.setText(Integer.toBinaryString(value))
+        octValue.setText(Integer.toOctalString(value))
+        decValue.setText(Integer.valueOf(value).toString())
+    }
+
 }
--- app/src/main/java/com/example/calculator/controller/TranActivity.kt
+++ app/src/main/java/com/example/calculator/controller/TranActivity.kt
@@ -3,15 +3,80 @@ package com.example.calculator.controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.calculator.R
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.EditText
+import java.math.BigDecimal
+
 
 class TranActivity: AppCompatActivity() {
+    lateinit var lengthValue: EditText
+    lateinit var volumeValue:EditText
+    lateinit var lengthResult: TextView
+    lateinit var volumeResult:TextView
+    lateinit var lengthValues: RadioGroup
+    lateinit var lengthResults:RadioGroup
+    lateinit var volumeValues:RadioGroup
+    lateinit var volumeResults:RadioGroup
+    lateinit var length: BigDecimal
+    lateinit var volume: BigDecimal
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tran)
-
+        initComponents()
     }
 
+    private fun initComponents() {
+        lengthValue = findViewById(R.id.length_value_text)
+        volumeValue = findViewById<EditText>(R.id.volume_value_text)
+        lengthResult = findViewById(R.id.length_result_text)
+        volumeResult = findViewById<TextView>(R.id.volume_result_text)
+        lengthValues = findViewById(R.id.length_values)
+        lengthResults = findViewById<RadioGroup>(R.id.length_results)
+        lengthValues.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { group, checkedId ->
+            val input = BigDecimal(lengthValue.getText().toString())
+            when (checkedId) {
+                R.id.length_value_km -> length = input.multiply(BigDecimal("1000"))
+                R.id.length_value_m -> length = input
+                R.id.length_value_cm -> length = input.multiply(BigDecimal("0.01"))
+                R.id.length_value_mm -> length = input.multiply(BigDecimal("0.001"))
+            }
+        })
+        lengthResults.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { group, checkedId ->
+            val result: String
+            var temp = BigDecimal(0)
+            when (checkedId) {
+                R.id.length_result_km -> temp = length.multiply(BigDecimal("0.001"))
+                R.id.length_result_m -> temp = length
+                R.id.length_result_cm -> temp = length.multiply(BigDecimal("100"))
+                R.id.length_result_mm -> temp = length.multiply(BigDecimal("1000"))
+            }
+            result = temp.toString()
+            lengthResult.setText(result)
+        })
+        volumeValues = findViewById<RadioGroup>(R.id.volume_values)
+        volumeResults = findViewById<RadioGroup>(R.id.volume_results)
+        volumeValues.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { group, checkedId ->
+            val input = BigDecimal(volumeValue.getText().toString())
+            when (checkedId) {
+                R.id.volume_value_m2 -> volume = input.multiply(BigDecimal("1000"))
+                R.id.volume_value_dm2 -> volume = input
+                R.id.volume_value_cm2 -> volume = input.multiply(BigDecimal("0.001"))
+            }
+        })
+        volumeResults.setOnCheckedChangeListener(RadioGroup.OnCheckedChangeListener { group, checkedId ->
+            val result: String
+            var temp = BigDecimal(0)
+            when (checkedId) {
+                R.id.volume_result_m2 -> temp = volume.multiply(BigDecimal("0.001"))
+                R.id.volume_result_dm2 -> temp = volume
+                R.id.volume_result_cm2 -> temp = volume.multiply(BigDecimal("1000"))
+            }
+            result = temp.toString()
+            volumeResult.setText(result)
+        })
+    }
 
 
 }
